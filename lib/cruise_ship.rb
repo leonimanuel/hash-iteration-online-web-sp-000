cfea523ea8fb4de3c7afeaec301f4057c9b714cc
Leon Malisov <leonmalisov@gmail.com>
2020-01-13 16:45:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,13 +1,15 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
   # add the code snippet here!
   passengers.detect {|suite, passenger| suite == "suite_a" ||
     passenger[0] == "A"}
 end
+
+binding.pry
